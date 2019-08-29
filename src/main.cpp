af1097822ec370f53a6068f64049c09820a0d6ee
jeferson0101 <jefersongomes0101@gmail.com>
2019-08-28 21:51:32 -0300

    Adding new sensors

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,10 @@
 #include <DHT_U.h>
 // todo sensor_adafruit precisa dessa biblioteca
 #include <Adafruit_Sensor.h>
+// bibliotecas barômetro
+#include <Wire.h>
+
+#include <Adafruit_BMP280.h>
 
 //-----------------------------------GSM------------------------------
 
@@ -26,7 +30,7 @@ const char pass[] = "vivo";
 
 TinyGsm modem(Serial1);
 TinyGsmClient client(modem);
-uint8_t gsmState =0;
+uint8_t gsmState = 0;
 
 //-------------Configurações broker, mqtt e credenciais--------------
 PubSubClient mqtt(client);
@@ -64,7 +68,7 @@ int winddir = 0;
 // ---------------------Anemometro------------------------------
 // --- Constantes ---
 const float pi = 3.14159265;     //Número de pi
-int period = 5000;               //Tempo de medida(miliseconds)
+unsigned long period = 5000;               //Tempo de medida(miliseconds)
 int delaytime = 2000;            //Invervalo entre as amostras (miliseconds)
 int radius = 115;                //Raio do anemometro(mm)
 uint8_t pinAnem = 5;
@@ -73,15 +77,35 @@ unsigned int counter = 0;        //Contador para o sensor
 unsigned int RPM = 0;            //Rotações por minuto
 float speedwind = 0;             //Velocidade do vento (m/s)
 float windspeed = 0;             //Velocidade do vento (km/h)
-//--------------------------------------------------------------------
+//----------------------SensorCargaBateria------------------------------
+int value = 0;
+float voltage;
+float R1 = 5700.0;  // 0.98 +  4.64 = 5.62
+float R2 = 2200.0; // 2.18
+//---------------------Umidade do solo------------------------------
+#define pino_sinal_analogico A3
+int humidity_value;
 
+//------------------------Barômetro-Pressão_atmosférica---------------------------
+
+Adafruit_BMP280 bmp; // I2C
+
+//--------------------Sensor de Chuva---------------------------------------
+#define pinSensorA A1
+
+//---------------------------------------------------------------------------
 //Declarando os métodos para ficarem visíveis ao método principal
 void sendDhtData();
 void portaHandle();
 void sendPluvioData();
+void sendBatteryCharge();
 void sendAnemometerData();
 void sendEndDirection();
 void sendData();
+void sendGroundHumidity();
+void sendRainSensor();
+void sendAtmosphericPressure();
+
 boolean mqttConnect();
 
 // - anemometro metodos
@@ -97,16 +121,33 @@ long lastReconnectAttempt = 0;
 
 void setup() {
 
+ 
+  Serial.begin(9600);
+
+  //Comunicação GSM RX TX (0,1)
+  Serial1.begin(9600);
+
+  //sensor de chuva
+  pinMode(pinSensorA, INPUT);
+
+   // Barômetro
+   if (!bmp.begin(0x76)) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
   last_send=0;
 
   // -- anemometro
   pinMode(pinAnem, INPUT_PULLUP);        //configura o digital 2 como entrada
 
 
-  Serial.begin(19200);
-
-  //Comunicação GSM RX TX (0,1)
-  Serial1.begin(19200);
 
   if(gsmState == 0){
     //ligar o GPRS automaticamente sem precisar pressionar o botão.
@@ -125,35 +166,35 @@ void setup() {
 
   // Serial.println("Initializing modem...");
   // modem.restart();
-  //
+
   // String modemInfo = modem.getModemInfo();
   // Serial.print("Modem: ");
   // Serial.println(modemInfo);
 
   // //Unlock your SIM card with a PIN
   // //modem.simUnlock("1234");
-  //
+
 
 
   // Serial.print("Waiting for network...");
   // while (!modem.waitForNetwork()) {
   //   Serial.println(" fail");
-  //
+
   // }
   // Serial.println(" OK");
-  //
+
   // Serial.print("Connecting to ");
   // Serial.print(apn);
   // while(!modem.gprsConnect(apn, user, pass)) {
   //   Serial.println(" fail");
-  //
+
   //   Serial.print("Connecting to ");
   //   Serial.print(apn);
-  //
+
   //   // while (true);
   // }
   // Serial.println(" OK");
-  //
+
   // // MQTT Broker setup
   // mqtt.setServer(broker, mqttPort);
 
@@ -164,14 +205,14 @@ void loop() {
 
 
    if(millis() - last_send > 500){
-    if(mqtt.connected()){
+    // if(mqtt.connected()){
 
-    }else{
-      Serial.println("Connectin to server..");
-      while(!mqttConnect()){
-        mqttConnect();
-      }
-    }
+    // }else{
+    //   Serial.println("Connectin to server..");
+    //   while(!mqttConnect()){
+    //     mqttConnect();
+    //   }
+    // }
 
     sendData();
      last_send = millis();
@@ -182,7 +223,7 @@ void loop() {
 
 }
 
-// if (!mqttConnect()) {
+// if(!mqttConnect()){
 //   Serial.println();
 //   Serial.println("=== MQTT NOT CONNECTED ===");
 //   // Reconnect every 10 seconds
@@ -221,8 +262,16 @@ void loop() {
     Serial.print(" graus");
 
 
-    doc["endDirection"]= winddir;
+    doc["endDirection"]= winddir; 
   }
+
+  void sendBatteryCharge(){
+    value = analogRead(A0);
+    Serial.print("valorBateria");
+    Serial.println(value);
+    voltage = value * (3.3/1024)*((R1 + R2)/R2);
+    doc["BatteryCharge"]= voltage;
+  } 
 
   void sendAnemometerData(){
     Sample++;
@@ -251,6 +300,37 @@ void loop() {
     Serial.println();
 
     doc["Anemometer"] = speedwind;
+  }
+
+  void sendGroundHumidity(){
+    humidity_value = analogRead(pino_sinal_analogico);
+
+    //Mostra o valor da porta analogica no serial monitor
+    Serial.print("Porta analogica: ");
+    Serial.print(humidity_value);
+
+    //Solo umido, acende o led verde
+    if (humidity_value > 0 && humidity_value < 610)
+    {
+      Serial.println(" Status: Solo umido");
+      doc["UmidadeSolo"] = "Umido";
+
+    }
+
+    //Solo com umidade moderada, acende led amarelo
+    if (humidity_value > 610 && humidity_value < 800)
+    {
+      Serial.println(" Status: Umidade moderada");
+      doc["UmidadeSolo"] = "Moderada";
+    }
+
+    //Solo seco, acende led vermelho
+    if (humidity_value > 800 && humidity_value < 1024)
+    {
+      Serial.println(" Status: Solo seco");
+      doc["UmidadeSolo"] = "Seco";
+
+    }
   }
 
   void sendPluvioData(){
@@ -290,6 +370,10 @@ void sendData(){
   sendPluvioData();
   sendAnemometerData();
   sendEndDirection();
+  sendGroundHumidity();
+  sendRainSensor();
+  sendBatteryCharge();
+  sendAtmosphericPressure();
 
 
   serializeJson(doc, payload);
@@ -330,7 +414,7 @@ void windvelocity()
   counter = 0;
   attachInterrupt(pinAnem, addcount, RISING);
   unsigned long millis();
-  long startTime = millis();
+  unsigned long startTime = millis();
   while(millis() < startTime + period) {}
 }
 
@@ -362,6 +446,32 @@ void SpeedWind()
 void addcount()
 {
   counter++;
+}
+
+// Sensor de Chuva
+void sendRainSensor(){
+  if (analogRead(pinSensorA) > 700) {
+    doc["sensorChuva"] = 0;
+    //Serial.println("Não está chovendo");
+     
+  } else {
+    doc["sensorChuva"]= 1;
+    //Serial.println("Chovendo");
+  }
+}
+
+//pressão atmosférica
+void sendAtmosphericPressure(){
+  
+  doc["pressure"] = bmp.readPressure()/100; // já fazendo conversão de Pa -> hPa
+  doc["altitude"] = bmp.readAltitude(1013.25);
+  // Serial.print(F("Pressure = "));
+  // Serial.print(bmp.readPressure());
+  // Serial.println(" hPa");
+
+  // Serial.print(F("Approx altitude = "));
+  // Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+  // Serial.println(" m");
 }
 
 // pegar valor pluviômetro
